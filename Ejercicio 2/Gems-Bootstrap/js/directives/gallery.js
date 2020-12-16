c77531acc1f877650b0a16ab0a5b237e55859f4c
OsmanTarango <61672796+OsmanTarango@users.noreply.github.com>
2020-12-15 20:33:49 -0700

    Added Ejercicio 2
    :D

---
+++ Ejercicio 2/Gems-Bootstrap/js/directives/gallery.js
@@ -0,0 +1,21 @@
+(function () {
+
+    let app = angular.module('gallery', [])
+
+    app.directive('productGallery', function() {
+        return{
+            restrict: 'E',
+            templateUrl: 'partials/product-gallery.html',
+            controller: function() {
+                this.current = 0
+                this.setCurrent = function(i) {
+                    this.current = i || 0
+                }
+                this.isSet = function (i) {
+                    return i === this.current
+                }
+            },
+            controllerAs: 'gallery'
+        }
+    })
+})();
